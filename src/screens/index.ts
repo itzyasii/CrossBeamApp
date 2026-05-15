f5cc2881512acddcf255236092d8b9d9d8e66535
Yasir Ali ✅ <Yasirpechuho1@gmail.com>
2026-05-16 01:52:56 +0500

    Add screens index

---
+++ src/screens/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Screens index
+ */
+
+export { DeviceDiscoveryScreen } from './DeviceDiscoveryScreen';
+export { TransferScreen } from './TransferScreen';
+export { TransferHistoryScreen } from './TransferHistoryScreen';
+export { SettingsScreen } from './SettingsScreen';
